418a84a31a200bd240eaa0fdc1616338bdc03df8
noRan56 <noran.nassef12@gmail.com>
2024-10-14 08:20:30 +0300

    - fonts & colors

--- lib/core/styling/app_text_styles.dart
+++ lib/core/styling/app_text_styles.dart
@@ -1,1 +1,72 @@
-class AppTextStyles {}
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo/core/styling/font_weight_helper.dart';
+import 'package:todo/core/styling/my_color.dart';
+
+class AppTextStyles {
+  static TextStyle font24BlackBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: Colors.black,
+  );
+
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle font13BlueSemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle font16WhiteSemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.semiBold,
+    color: Colors.white,
+  );
+
+  static TextStyle font13GrayRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font12GrayRegular = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font12GrayMedium = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: AppColors.grey,
+  );
+
+  static TextStyle font12BlueRegular = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle font13BlueRegular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+    color: AppColors.primaryColor,
+  );
+
+  static TextStyle font18WhiteMedium = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: Colors.white,
+  );
+}
--- lib/core/styling/my_color.dart
+++ lib/core/styling/my_color.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 class AppColors {
   static const Color primaryColor = Color(0xff4568dc);
   static const Color white = Color.fromARGB(255, 255, 255, 255);
+  static const Color grey = Color.fromARGB(255, 171, 169, 169);
   static const List<Color> primaryGradientColor = [
     Color(0xff4568dc),
     Color(0xffb06ab3)
