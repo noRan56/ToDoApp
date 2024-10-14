309d9baa50dcaed5eba325b36d158c24b243ed29
noRan56 <noran.nassef12@gmail.com>
2024-10-14 07:00:21 +0300

    - adding styling

--- lib/core/styling/my_color.dart
+++ lib/core/styling/my_color.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 class AppColors {
   static const Color primaryColor = Color(0xff4568dc);
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
   static const List<Color> primaryGradientColor = [
     Color(0xff4568dc),
     Color(0xffb06ab3)
